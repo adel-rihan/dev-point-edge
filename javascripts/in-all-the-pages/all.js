a9b94d4fa61b1503a1e200c54da661f64ab9ab80
Adel Rehan <adelaaa93@yahoo.com>
2016-08-27 16:50:43 +0200

    dev-point-edge

--- javascripts/in-all-the-pages/all.js
+++ javascripts/in-all-the-pages/all.js
@@ -239,14 +239,14 @@ $(function() {
 $(function() {
   if (_userdata.user_id == 1 && !my_getcookie('fae_update_alerted')) {
 
-    $.get('https://raw.githubusercontent.com/SethClydesdale/forumactif-edge/master/javascripts/version-data.js', function(d) {
+    $.get('https://raw.githubusercontent.com/adel759/dev-point-edge/master/javascripts/version-data.js', function(d) {
       var script = document.createElement('SCRIPT');
       script.type = 'text/javascript';
       script.text = d.replace(/forumactif_edge_version_data/, 'fae_github_version_data');
       document.body.appendChild(script);
 
       if (forumactif_edge_version_data.length < fae_github_version_data.length) {
-        alert('An update has been found for Forumactif Edge. When you\'re ready, please proceed to your FAE Control Panel and click "Check for updates" to begin the update process.');
+        alert('An update has been found for Dev-Point Edge. When you\'re ready, please proceed to your DPE Control Panel and click "Check for updates" to begin the update process.');
       }
 
       my_setcookie('fae_update_alerted', '1');
